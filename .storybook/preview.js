1fd0ac45c7ed569ca1ea98ac415f337bef1b751a
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-09-11 11:40:04 +0200

    Revert "feat(content pages): content pages"

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -17,43 +17,18 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-/********************************************************************************
- * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
-
 import React from 'react'
 import { SharedThemeProvider } from '../src/components/basic/SharedThemeProvider'
 import { SharedCssBaseline } from '../src/components/basic/SharedCssBaseline'
-import { I18nextProvider } from 'react-i18next'
-import i18n from '../src/i18n'
 
-const withI18next = (Story) => {
-  return (
+export const decorators = [
+  (Story) => (
     <SharedThemeProvider>
       <SharedCssBaseline />
-      <I18nextProvider i18n={i18n}>
-        <Story />
-      </I18nextProvider>
+      <Story />
     </SharedThemeProvider>
-  );
-};
-
-export const decorators = [withI18next];
+  ),
+]
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
