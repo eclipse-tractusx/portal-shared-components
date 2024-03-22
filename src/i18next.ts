eed891438200a05c087c396937d6a9eac414cad1
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2024-03-22 10:56:00 +0100

    chore(sonar): fix sonar finding

--- src/i18next.ts
+++ src/i18next.ts
@@ -20,17 +20,17 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-void i18n
-  // pass the i18n instance to react-i18next.
+i18n
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
     debug: true,
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     },
+  })
+  .catch((e: unknown) => {
+    console.error('i18n error:', e)
   })
 
 export default i18n
