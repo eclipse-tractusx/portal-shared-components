e22bc38631321e1b0f5a3011946b67b8eccd9aaf
manojava-gk <ibm.g_koushik@mercedes-benz.com>
2024-06-25 15:48:15 +0530

    fix(mdx): remove .md files and include the details in respective mdx file

--- src/components/basic/Datepicker/Datepicker.stories.tsx
+++ src/components/basic/Datepicker/Datepicker.stories.tsx
@@ -22,18 +22,12 @@ import { ComponentStory } from '@storybook/react'
 import { Box } from '@mui/material'
 
 import { Datepicker as Component } from '.'
-import datepicker from '../../../../docs/storybook/datepicker.md?raw'
 
 export default {
   title: 'Datepicker',
   component: Component,
   tags: ['autodocs'],
   argTypes: {},
-  parameters: {
-    docs: {
-      description: { component: datepicker },
-    },
-  },
 }
 
 const Template: ComponentStory<typeof Component> = (args: any) => (
