2f0bd8f922d54cd712a6d2fe99b57d26072ead9f
kunalgaurav-bmw <kunal.gaurav@partner.bmw.in>
2024-09-13 11:44:16 +0530

    fix(new toggle switch): default text removed and ui issue fixed on hard reload

--- src/components/basic/ToggleSwitch/ToggleSwitch.stories.tsx
+++ src/components/basic/ToggleSwitch/ToggleSwitch.stories.tsx
@@ -39,8 +39,8 @@ const Template: ComponentStory<typeof Component> = (
   return <Component {...args} checked={checked} onChange={handleToggleChange} />
 }
 
-export const Default = Template.bind({})
-Default.args = {
+export const ToggleSwitch = Template.bind({})
+ToggleSwitch.args = {
   disabled: false,
-  isHoverEnabled: false,
+  hoverEnabled: false,
 }
